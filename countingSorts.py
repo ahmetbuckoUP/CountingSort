3608d56be2c887da39e91be078d8ce51031fc4e4
ahmetbuckoUP <75639075+ahmetbuckoUP@users.noreply.github.com>
2020-12-14 22:32:24 +0100

    Counting Sort in same file as DEF function

---
+++ countingSorts.py
@@ -0,0 +1,59 @@
+
+#Comparison Counting Sort
+def comparisonCountingSort(list):
+    # list = [13, 6, 9, 3, 10, 7]
+    dataDict = {}
+    sortedList = []
+    count = 0
+    for i in range(0, len(list)):
+        for j in range(0, len(list)):
+            if list[i] > list[j]:
+                # print(list[i])
+                count = count + 1
+        d = {count: list[i]}
+        dataDict.update(d)
+        count = 0
+    for i in range(0, len(list)):
+        sortedList.append(dataDict[i])
+    print('Unsorted List {}'.format(list))
+    print('Sorted Array with Comparison Counting Sort {}'.format(sortedList))
+
+#Distribuiton Counting Sort
+def DistributinCountingSort(list):
+    # list = [9, 6, 9, 9, 6, 5]
+    dataDict = {}
+    unique = set(list)
+    unique = sorted(unique)
+    distributionValues = {}
+    sortedList = []
+    count = 0
+    for i in range(0, len(list)):
+        for j in range(0, len(list)):
+            if list[i] == list[j]:
+                count = count + 1
+        encounter = {list[i]: count}
+        dataDict.update(encounter)
+        dataDict = dict(sorted(dataDict.items(), key=lambda item: item[0]))
+        count = 0
+    print('Encounter Times{}'.format(dataDict))
+    j = 0
+    for i in unique:
+        j = j + dataDict[i]
+        dValues = {i: j}
+        distributionValues.update(dValues)
+    print('Distributed Values {}'.format(distributionValues))
+    for i in dataDict.keys():
+        for j in range(0, dataDict[i]):
+            if dataDict[i] == 0:
+                break
+            else:
+                sortedList.append(i)
+                dataDict[i] = dataDict[i] - 1
+                continue
+    print('Sorted List with Distribution Counting Sort {}'.format(sortedList))
+
+
+print('\n<----------Comparison Counting Sort--------->\n')
+comparisonCountingSort([13, 6, 9, 3, 10, 7])
+print('\n<----------Distribution Counting Sort--------->\n')
+DistributinCountingSort([9, 6, 9,21,21, 9, 6, 5])
